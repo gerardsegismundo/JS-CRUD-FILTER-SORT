4008ca8d5ded78e9615a819662f94b2414026205
Bullet <gerardmartinsegismundo@gmail.com>
2021-09-03 18:47:23 -0700

    modify href path

--- js/index.js
+++ js/index.js
@@ -45,20 +45,12 @@ class UI {
     const userIdV = targetId.replace('view-', '')
     createUserCookie(userIdV)
 
-    if (window.location.href.includes('JS-CRUD-FILTER-SORT')) {
-      return (window.location.href = '/JS-CRUD-FILTER-SORT/user.html')
-    }
-
     window.location.href = '/user.html'
   }
 
   static editUser(targetId) {
     const userIdE = targetId.replace('edit-', '')
     createUserCookie(userIdE)
-
-    if (window.location.href.includes('JS-CRUD-FILTER-SORT')) {
-      return (window.location.href = '/JS-CRUD-FILTER-SORT/edit.html')
-    }
 
     window.location.href = '/edit.html'
   }
